4a7d7ed11764a112998b11074e3c114d11a8d921
unknown <20it111@charusat.edu.in>
2022-09-13 22:35:36 +0530

    Student Listing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
+      
       <NavigationBar/>
+      <Container>
       <Student/>
       <StudentList/>
+      </Container>
+      <Footer/>
     </div>
   );
 }
